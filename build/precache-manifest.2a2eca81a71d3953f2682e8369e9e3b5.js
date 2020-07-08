e8f693398089d2fa33491281e11b5b65e4db59b1
Thucnv <nguyenvanthuc1912@gmail.com>
2020-07-08 10:19:32 +0700

    first commit

---
+++ build/precache-manifest.2a2eca81a71d3953f2682e8369e9e3b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543e239aff595200a2cf5ce2c9b628db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdbc55bd6cf1e04dd4a4",
+    "url": "/static/css/main.38f82eaf.chunk.css"
+  },
+  {
+    "revision": "938b89b32fd4f0d75d2f",
+    "url": "/static/js/2.b978bf80.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.b978bf80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbc55bd6cf1e04dd4a4",
+    "url": "/static/js/main.3500d28d.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
